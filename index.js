8a3c5c8fc5d048eebdcb5465a2e1c465d8234364
Rohit Singh <103621904+rohit70705@users.noreply.github.com>
2023-07-07 14:00:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name : "Welcome to your todo List!"
+  name : "Welcome to your to-do List!"
 });
 const item2 = new Item({
   name : "Hit the + button to add a new item."
 });
 const item3 = new Item({
-  name : "<--- Hit this item t0 delete an item."
+  name : "<--- Hit this item to delete an item."
 });
 
 
